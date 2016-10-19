7cae73652bfaf17c3d0ba6d515c2e7637cd0369b
amglobal99 <amglobal99@yahoo.com>
2016-10-19 14:04:36 -0400

    finished Optionals  3 section

--- MyPlayground.playground/Pages/Optionals_3.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Optionals_3.xcplaygroundpage/Contents.swift
@@ -18,8 +18,15 @@ struct Politician {
         self.height = height
     }
     
-    // ======== Optional Mehod =================
+    
+    // ======== Optional Mehods =================
     func getBalance(height:Int) -> Int? {
+        let bal:Int? = getBal(height: height)
+        return bal
+    } // end func
+    
+    
+    func getBal(height:Int) -> Int? {
         var bal:Int? = 0
         if height == 60 {
             bal = 1000
@@ -29,14 +36,14 @@ struct Politician {
             bal = nil
         }
         return bal
-    } // end func
+     }  // end func
     
-    
+  
 }  // end struct
 
 
 
-
+// =========  define a struct for Country ======================
 struct Country {
     
     var name:String
@@ -63,9 +70,7 @@ struct Country {
             return "Pune"
         default:
             return nil
-            
         } // end switch
-        
     }  // end func
     
     
@@ -92,14 +97,24 @@ struct Country {
 
 
 var neta:Politician? = Politician(color:"black",height:40)
-
-var myCountry = Country(name:"India",pop:2000,pm:neta!)
-
-
-
-var netaBalance:Int?  = myCountry.pm?.getBalance(height: 100)
+//var neta:Politician? = nil
+var myCountry = Country(name:"India",pop:2000,pm: neta!)
 
 
+// NOTE: on below line, pm is a property of myCountry.
+// And it is an Ooptional.
+// So you put a ? after that variable
+//
+// SO REMEMBER: When you are using any property, and thta property is optional, put ? after its name
+
+// Additional Comments: 
+// The last part is a method call to getBalance.
+// This Method returns optional. You don't need to put ? after its name.
+// You put the "?" after variable on left hand side.
+// The value of the method will be assigned to var on left. To indicate it can be nil, place a "?"
+// If you remove the ? after Int on left, you get an error
+
+var netaBalance:Int? = myCountry.pm?.getBalance(height: 60)
 
 
 
