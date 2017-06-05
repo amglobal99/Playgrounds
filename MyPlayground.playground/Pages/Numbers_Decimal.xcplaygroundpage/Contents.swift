d6deceb6725528d36e282732789a7cf4e019329d
jackseapine <jack@seapineva.com>
2017-06-05 11:57:22 -0400

    minor update

--- MyPlayground.playground/Pages/Numbers_Decimal.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Numbers_Decimal.xcplaygroundpage/Contents.swift
@@ -3,21 +3,57 @@
 import Foundation
 
 
+var ttt:Double = 15.0
+var mm = (ttt/100)
+
+print(mm)
+
+
+
+
 var x:Int = 51575
+var zzz = x * 100
+
+
+var minInt = Int.max
+print(minInt)
+
+
+var cur:Decimal = 12.50
+var jp = cur * 100
+
+for _ in 1...10 {
+  cur += 0.05
+}
+
+print("Cur is now \(cur) ")
+
+
+
+
+
+var y = Double(x)/100
+
+print(y)
+
+
 
 var am = ( Double(x)/100 ).rounded()
-
 
 print (am)
 
 
 
 
+
+
+
 /// For accuracy use NSDecialNumber
 let amt:Double = 0.0
-var sum:NSDecimalNumber = NSDecimalNumber(value: amt)
+var sum:Decimal = Decimal(amt)
 for _ in 1...100  {
-  sum = sum.adding(0.01)
+  //sum = sum.adding(0.01)
+  sum = sum.advanced(by: 0.01)
 }
 
 
